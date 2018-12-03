7b798b988aa3153c12f51bb2e474a3d7c7d7e5f1
RinatValiullov <4rontender@gmail.com>
2018-12-03 20:15:20 +0500

    finished left half

--- js/code.js
+++ js/code.js
@@ -1,1 +1,19 @@
-console.log('just for check');
+var jqBar = jQuery(".skill_block");
+var jqBarStatus = true;
+jQuery(window).scroll(function () {
+    var scrollEvent =
+        jQuery(window).scrollTop() >
+        jqBar.position().top - jQuery(window).height();
+    if (scrollEvent && jqBarStatus) {
+        jqBarStatus = false;
+        jQuery(".skill_block__value").each(function () {
+            jQuery(this)
+                .find(".skill_block__bar")
+                .animate({
+                        width: jQuery(this).attr("data-percent")
+                    },
+                    4000
+                );
+        });
+    }
+});
